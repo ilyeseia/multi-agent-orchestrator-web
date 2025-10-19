ca91dfbd2eac4dc147d46896d63c821c85205c79
ilyeseia <42816665+ilyeseia@users.noreply.github.com>
2025-10-19 20:30:21 +0100

    Create celery.py

---
+++ app/celery.py
@@ -0,0 +1,17 @@
+from celery import Celery
+from .config import Config
+
+celery_app = Celery(
+    "orchestrator",
+    broker=Config.CELERY_BROKER_URL,
+    backend=Config.CELERY_RESULT_BACKEND
+)
+
+# تحديث إعدادات Celery باستخدام إعدادات Flask
+celery_app.conf.update(
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json'],
+    timezone='UTC',
+    enable_utc=True
+)
